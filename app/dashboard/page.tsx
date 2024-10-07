fc7954a4aa9fcda0c4645eb1dadcf14655da4cee
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-10-07 21:52:44 +0530

    [UPD] Landing page UI completed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,50 @@
-import { redirect } from "next/navigation";
-import { auth } from "../lib/auth";
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../components/EmptyState";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      userName: true,
+      eventType: {
+        select: {
+          id: true,
+          active: true,
+          url: true,
+          title: true,
+          duration: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+}
 
 export default async function DashboardPage() {
   const session = await requireUser();
+  const data = await getData(session.user?.id as string);
 
-  return <h1>Hello from Dashboard</h1>;
+  return (
+    <>
+      {data.eventType.length === 0 ? (
+        <EmptyState
+          title="You have no Event Types"
+          description="You can create your first event type by clicking the button below"
+          buttonText="Create Event Type"
+          href="/dashboard/new"
+        />
+      ) : (
+        <p>There is data</p>
+      )}
+    </>
+  );
 }
