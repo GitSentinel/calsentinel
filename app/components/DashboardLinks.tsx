bb530efa9e262c07e2ac30abc1b8592e8f8aaedd
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-10-14 18:44:25 +0530

    [UPD]: Styles updated

--- app/components/DashboardLinks.tsx
+++ app/components/DashboardLinks.tsx
@@ -59,7 +59,7 @@ export function DashboardLinks() {
             pathname == link.href
               ? "text-primary bg-primary/10"
               : "text-muted-foreground hover:text-foreground",
-            "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
+            "flex items-center gap-3 rounded-lg px-3 py-6 transition-all hover:text-primary my-5 text-md"
           )}
           key={link.id}
           href={link.href}
